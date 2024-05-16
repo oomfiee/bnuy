cfa95ad5a75de94062954e167bd22164190f7edc
oomfie <dev.77r2m@simplelogin.co>
2024-05-16 20:28:27 +0300

    change

--- config/scripts/kernel.sh
+++ config/scripts/kernel.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S nu
 use lib/std.nu [get_fedora_version, fetch_copr]
 let FEDORA_MAJOR_VERSION = (get_fedora_version)
-let COPR_FILE = (fetch_copr  $"https://copr.fedorainfracloud.org/coprs/whitehara/kernel-tkg/repo/fedora-($FEDORA_MAJOR_VERSION)/whitehara-kernel-tkg-fedora-($FEDORA_MAJOR_VERSION).repo")
+let COPR_FILE = (fetch_copr  $"https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-($FEDORA_MAJOR_VERSION)/bieszczaders-kernel-cachyos-fedora-($FEDORA_MAJOR_VERSION).repo")
 
 rpm-ostree cliwrap install-to-root /
-rpm-ostree override replace --experimental --from repo='copr:copr.fedorainfracloud.org:whitehara:kernel-tkg' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-uki-virt
+rpm-ostree override replace --experimental --from repo='copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-uki-virt
 
 rm -f $COPR_FILE
