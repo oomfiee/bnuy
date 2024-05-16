d3c95075d04f2ec07c1937d63e4893c2fafcc7bf
oomfie <dev.77r2m@simplelogin.co>
2024-05-16 20:33:14 +0300

    change kernel

--- config/scripts/kernel.sh
+++ config/scripts/kernel.sh
@@ -4,6 +4,6 @@ let FEDORA_MAJOR_VERSION = (get_fedora_version)
 let COPR_FILE = (fetch_copr  $"https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-($FEDORA_MAJOR_VERSION)/bieszczaders-kernel-cachyos-fedora-($FEDORA_MAJOR_VERSION).repo")
 
 rpm-ostree cliwrap install-to-root /
-rpm-ostree override replace --experimental --from repo='copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-uki-virt
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos
 
 rm -f $COPR_FILE
