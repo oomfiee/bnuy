068b8efbe780c902829d1178690fbeee07dd9b7b
oomfie <dev.77r2m@simplelogin.co>
2024-08-28 13:05:42 +0300

    GO ALL IN!

---
+++ files/scripts/nvidia.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+rpm-ostree kargs \
+    --append=rd.driver.blacklist=nouveau \
+    --append=modprobe.blacklist=nouveau \
+    --append=nvidia-drm.modeset=1
+
+# specific Initramfs fixes
+echo "options nvidia NVreg_TemporaryFilePath=/var/tmp" >> /usr/lib/modprobe.d/nvidia-atomic.conf
+cp /etc/modprobe.d/nvidia-modeset.conf /usr/lib/modprobe.d/nvidia-modeset.conf
+sed -i 's@omit_drivers@force_drivers@g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
